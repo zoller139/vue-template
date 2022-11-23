8a36b62e9b4da65b7c66b972b8952a18f9e07c71
zhy <zhy90655@163.com>
2022-11-23 09:30:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import { useMouse } from './components/bar'
+const dd = reactive({ cs: 1 })
+const id = ref(0)
+console.log(1111)
+const { x, y } = useMouse()
+function hddd() {
+  console.log(dd)
+  console.log(id)
+}
+</script>
+
+<template>
+  <div>
+    {{dd.cs}}
+    <button @click="dd.cs++">add</button>
+    <button @click="hddd">add</button>
+    <p>{{y}} {{x}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
